bf849fc54b95055d49dc7a538d5874af25e80de9
Hoang Huyen <1851061774@e.tlu.edu.vn>
2021-11-05 15:18:47 +0700

    option dung anh gif cho PC

---
+++ assets/js/home.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function(){
+    $('.your-class').slick({
+        autoplay:true,
+        // slidesToShow: 3,
+        // slidesToScroll: 1
+    });
+  });
+
+  $(document).ready(function(){
+    $('.autoplay').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+  });
+ 
